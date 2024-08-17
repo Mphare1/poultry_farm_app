d649679c65a1cec6dac07c653129ffe5ca123822
Mphare1 <thipanemp@gmail.com>
2024-08-17 23:38:18 +0200

    add bcyypt to hashpassword

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,21 +1,28 @@
 const User = require("../model/user.model");
+const bcryptjs = require('bcryptjs');
 
 const signup = async (req, res) => {
  const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({ message: 'All fields are required' });
+        res.status(400).json({ message: 'All fields are required' });
     }
+
+    const hashPassword = bcrypt.hashSync(password, 10);
 
     const newUser = new User({
         username,
         email,
         password,
     });
-
+    try {
     await newUser.save();
-    return res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json({ message: 'User created successfully' });
 }
+ catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+}
+};
 
 
 module.exports = { signup };
